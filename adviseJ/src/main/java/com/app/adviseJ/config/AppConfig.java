1a5cac9cffa09d9dafbf574cde969f3699a69dcb
Jonasz <Jonasz@Jonasz>
2014-09-03 18:45:31 +0200

    up to date

---
+++ adviseJ/src/main/java/com/app/adviseJ/config/AppConfig.java
@@ -0,0 +1,58 @@
+package com.app.adviseJ.config;
+
+import java.util.Properties;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+ 
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.app.adviseJ.*" })
+@EnableTransactionManagement
+@Import({ SecurityConfig.class })
+public class AppConfig {
+ 
+        @Bean
+        public SessionFactory sessionFactory() {
+                LocalSessionFactoryBuilder builder = 
+			new LocalSessionFactoryBuilder(dataSource());
+                builder.scanPackages("com.app.adviseJ.users.model")
+                      .addProperties(getHibernateProperties());
+ 
+                return builder.buildSessionFactory();
+        }
+ 
+	private Properties getHibernateProperties() {
+                Properties prop = new Properties();
+                prop.put("hibernate.format_sql", "true");
+                prop.put("hibernate.show_sql", "true");
+                prop.put("hibernate.dialect", 
+                    "org.hibernate.dialect.MySQL5Dialect");
+                return prop;
+        }
+ 
+	@Bean(name = "dataSource")
+	public BasicDataSource dataSource() {
+ 
+		BasicDataSource ds = new BasicDataSource();
+	        ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/radca_spring");
+		ds.setUsername("root");
+		ds.setPassword("9dqnge23");
+		return ds;
+	}
+ 
+	//Create a transaction manager
+	@Bean
+        public HibernateTransactionManager txManager() {
+                return new HibernateTransactionManager(sessionFactory());
+        }
+}
---
+++ adviseJ/src/main/java/com/app/adviseJ/users/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.app.adviseJ.users.dao;
+
+import com.app.adviseJ.users.model.User;
+
+public interface UserDao {
+ 
+	User findByUserName(String username);
+ 
+}
